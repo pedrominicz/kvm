b97ac15cbd1fbbd161a43a514523c652b5ce0c84
Pedro Minicz <pedrominicz@gmail.com>
2019-08-01 20:31:00 -0300

    write working IVT from BIOS

--- bios/bios.c
+++ bios/bios.c
@@ -1,9 +1,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
+extern const uint16_t interrupts[256]; // Defined in "entry.S".
+
+static inline void outb(uint16_t port, uint8_t data) {
+  asm volatile ("out %0, %1" :: "a"(data), "d"(port));
+}
+
+void bios_interrupt(void) {
+  outb(0x3f8, 'b');
+}
+
 void bios_init(void) {
   for(size_t i = 0; i < 256; ++i) {
-    asm volatile ("mov %0, %%es:(%1)" :: "r"(0x1234), "r"(i * 4));
-    asm volatile ("mov %0, %%es:(%1)" :: "r"(0xabcd), "r"(i * 4 + 2));
+    asm volatile ("mov %0, %%es:(%1)" :: "r"(interrupts[i]), "r"(i * 4));
+    asm volatile ("mov %0, %%es:(%1)" :: "r"(0xf000), "r"(i * 4 + 2));
   }
 }
---
+++ tests/ints.asm
@@ -0,0 +1,8 @@
+bits 16
+        int 10
+        int 11
+        int 12
+        int 13
+        int 14
+        int 15
+        hlt
